e54b9a94a3a92d9dbde9bc99349a9a04275f64e7
Gabriel Cordeiro <cordeirodados@gmail.com>
2024-10-31 20:32:39 -0300

    Update categorias.py

--- app_pages/categorias.py
+++ app_pages/categorias.py
@@ -9,8 +9,12 @@ def app():
     acao = st.selectbox("Escolha a ação", ["Inserir", "Alterar", "Deletar"])
 
     if acao == "Inserir":
-        locais = visualizar_dados("SELECT local_id, nome FROM data_collect.db_datatv.locais")
-        local_id = st.selectbox("Selecione o Local", [local[0] for local in locais], format_func=lambda x: dict(locais)[x])
+        # Carregar locais e cidades
+        locais = visualizar_dados("SELECT local_id, cidade FROM data_collect.db_datatv.locais")
+        locais_dict = {local[0]: local[1] for local in locais}  # Mapeia 'local_id' para 'cidade'
+
+        # Selecionar o local exibindo o nome da cidade
+        local_id = st.selectbox("Selecione a Cidade", [local[0] for local in locais], format_func=lambda x: locais_dict.get(x, ""))
         tipo_categoria = st.text_input("Tipo da Categoria (ex: Aéreo, Carro, Hotel)")
         
         if st.button("Salvar Categoria"):
@@ -20,24 +24,24 @@ def app():
                 (categoria_id, local_id, tipo_categoria)
             )
 
-
     elif acao == "Alterar":
         # Selecionar categoria para editar
         categorias = visualizar_dados("SELECT * FROM data_collect.db_datatv.categorias")
         categoria_id = st.selectbox("Selecione a Categoria", [cat[0] for cat in categorias])
 
         if categoria_id:
-            # Carregar locais para o campo de seleção
-            locais = visualizar_dados("SELECT local_id, nome FROM data_collect.db_datatv.locais")
-            
+            # Carregar locais e cidades
+            locais = visualizar_dados("SELECT local_id, cidade FROM data_collect.db_datatv.locais")
+            locais_dict = {local[0]: local[1] for local in locais}
+
             # Pegar os valores da categoria selecionada para exibir no formulário
             categoria_selecionada = next((cat for cat in categorias if cat[0] == categoria_id), None)
             if categoria_selecionada:
                 local_id = st.selectbox(
-                    "Selecione o Local",
+                    "Selecione a Cidade",
                     [local[0] for local in locais],
                     index=[local[0] for local in locais].index(categoria_selecionada[1]),
-                    format_func=lambda x: dict(locais)[x]
+                    format_func=lambda x: locais_dict.get(x, "")
                 )
                 tipo_categoria = st.text_input("Tipo da Categoria", value=categoria_selecionada[2])
 
